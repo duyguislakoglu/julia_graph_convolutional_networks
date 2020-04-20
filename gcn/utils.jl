4d0a2a0c8f1c337d3487c1503c4b65032421c7a4
Duygu Sezen Islakoğlu <dislakoglu15@ku.edu.tr>
2020-04-20 22:50:56 +0300

    Performed cora and citeseer experiments.

--- gcn/utils.jl
+++ gcn/utils.jl
@@ -12,25 +12,16 @@ import scipy.sparse as sp
 from scipy.sparse.linalg.eigen.arpack import eigsh
 import sys
 import os
-
 def parse_index_file(filename):
     index = []
     for line in open(filename):
         index.append(int(line.strip()))
     return index
-
 def sample_mask(idx, l):
     mask = np.zeros(l)
     mask[idx] = 1
     return np.array(mask, dtype=np.bool)
-
-def save_sparse_csr(filename, array):
-    np.savez(filename, data=array.data, indices=array.indices, indptr=array.indptr, shape=array.shape)
-def load_sparse_csr(filename):
-    loader = np.load(filename + '.npz')
-    return sp.csr_matrix((loader['data'], loader['indices'], loader['indptr']), shape=loader['shape'])
-
-def load_data(dataset_str, atype):
+def load_data(dataset_str):
     names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
     objects = []
     for i in range(len(names)):
@@ -39,11 +30,9 @@ def load_data(dataset_str, atype):
                 objects.append(pkl.load(f, encoding='latin1'))
             else:
                 objects.append(pkl.load(f))
-
     x, y, tx, ty, allx, ally, graph = tuple(objects)
     test_idx_reorder = parse_index_file("data/ind.{}.test.index".format(dataset_str))
     test_idx_range = np.sort(test_idx_reorder)
-
     if dataset_str == 'citeseer':
         # Fix citeseer dataset (there are some isolated nodes in the graph)
         # Find isolated nodes, add them as zero-vecs into the right position
@@ -56,6 +45,11 @@ def load_data(dataset_str, atype):
         ty = ty_extended
 
     if dataset_str == 'nell':
+        def save_sparse_csr(filename, array):
+            np.savez(filename, data=array.data, indices=array.indices, indptr=array.indptr, shape=array.shape)
+        def load_sparse_csr(filename):
+            loader = np.load(filename + '.npz')
+            return sp.csr_matrix((loader['data'], loader['indices'], loader['indptr']), shape=loader['shape'])
         test_idx_range_full = range(allx.shape[0], len(graph))
         isolated_node_idx = np.setdiff1d(test_idx_range_full, test_idx_reorder)
         tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
@@ -64,12 +58,9 @@ def load_data(dataset_str, atype):
         ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
         ty_extended[test_idx_range-allx.shape[0], :] = ty
         ty = ty_extended
-
         features = sp.vstack((allx, tx)).tolil()
         features[test_idx_reorder, :] = features[test_idx_range, :]
-
         idx_all = np.setdiff1d(range(len(graph)), isolated_node_idx)
-
         if not os.path.isfile("data/{}.features.npz".format(dataset_str)):
             print("Creating feature vectors for relations - this might take a while...")
             features_extended = sp.hstack((features, sp.lil_matrix((features.shape[0], len(isolated_node_idx)))),
@@ -84,22 +75,17 @@ def load_data(dataset_str, atype):
     else:
         features = sp.vstack((allx, tx)).tolil()
         features[test_idx_reorder, :] = features[test_idx_range, :]
-
     adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
-
     labels = np.vstack((ally, ty))
-    labels[test_idx_reorder,
-     :] = labels[test_idx_range, :]
-
+    labels[test_idx_reorder, :] = labels[test_idx_range, :]
     idx_test = test_idx_range.tolist()
     idx_train = range(len(y))
     idx_val = range(len(y), len(y)+500)
 
-    return adj, np.transpose(features), labels, idx_train, idx_val, idx_test
+    return adj, np.transpose(features), labels, idx_train, idx_val, test_idx_reorder
     """
-
     adj, features, labels, idx_train, idx_val, idx_test = py"load_data"(dataset)
-
+    
     # Zero-indexing issue
     idx_train = idx_train .+ 1
     idx_val = idx_val .+ 1
@@ -112,9 +98,6 @@ def load_data(dataset_str, atype):
     (I, J, V) = scipy_sparse_find(features)
     # Zero-indexing issue
     features = sparse(I .+ 1, J .+ 1, V)
-
-    adj = convert(atype, adj)
-    features = convert(atype features)
 
     return adj, features, labels, idx_train, idx_val, idx_test
 end
