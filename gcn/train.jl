a516a94a2e3d2b3811972d8be3382c525356bd15
Duygu Sezen Islakoğlu <dislakoglu15@ku.edu.tr>
2020-04-20 19:17:53 +0300

    Performed all experiments

--- gcn/train.jl
+++ gcn/train.jl
@@ -1,12 +1,15 @@
 using Pkg
-for p in ["Knet", "Plots", "IterTools"]
+for p in ["Knet", "Plots", "IterTools", "PyCall", "ArgParse"]
     if !haskey(Pkg.installed(),p)
         Pkg.add(p);
     end
 end
+module train
+using Knet
 using DelimitedFiles
-using Knet: KnetArray, accuracy, progress, minibatch, cycle, adam, sgd
+using Knet: KnetArray, accuracy, progress, minibatch, cycle, adam, xavier_uniform, progress!
 using Plots
+using ArgParse
 using IterTools: ncycle, takenth, take
 using Base.Iterators: flatten
 using LinearAlgebra
@@ -14,50 +17,110 @@ using LinearAlgebra
 include("utils.jl")
 include("models.jl")
 
-# Load data
-adj, features, labels, idx_train, idx_val, idx_test = load()
+function mytrain!(model, data, epochs, lr, window_size)
+    early_stop_counter = 0
+    prev_val_loss = 0
+    iter = 0
 
-# TODO: take user inputs
-struct args
-    epochs
-    lr
-    weight_decay
-    hidden
-    pdrop
+    trnloss = []
+    valloss = []
+
+    flag = true
+
+    function task()
+
+        append!(trnloss, model(data))
+        v_loss = val_loss(model, data)
+        append!(valloss, v_loss)
+
+        if v_loss == prev_val_loss
+            early_stop_counter = early_stop_counter + 1
+        else
+            early_stop_counter = 0
+        end
+        if early_stop_counter == window_size
+            flag = false
+        end
+        iter = iter + 1
+        prev_val_loss = v_loss
+        return flag
+
+    end
+
+    training = adam(model, ncycle(data, epochs), lr=lr)
+    progress!(flag = task() for x in (x for (i,x) in enumerate(training)) if flag)
+    return 1:iter, trnloss, valloss
 end
 
-arguments = args(200, 0.01, 5e-4, 16, 0.5)
-
-# Model and optimizer
-model = GCN(size(features,1),
-            arguments.hidden,
-            size(labels,2),
-            adj,
-            arguments.pdrop)
-
-(g::GCN)(x,y) = nll(g(x)[:, idx_train], y[idx_train])
-#(g::GCN)(x,y) = nll(g(x)[:, idx_train], y[idx_train]) + (arguments.weight_decay * sum(g.layer1.w .* g.layer1.w)) 
-
-output = model(features)
-labels_decoded = mapslices(argmax, labels ,dims=2)[:]
-loss = nll(output[:,idx_train], labels_decoded[idx_train])
-acc = accuracy(output[:,idx_train], labels_decoded[idx_train])
-using AutoGrad
-J = @diff model(features, labels_decoded)
-gr_check = grad(J, params(model)[1])
-
-dtrn =  minibatch(features, labels_decoded[:], length(labels_decoded))
-function train_with_results(model, dtrn, epoch, lr)
-    training = adam(model, ncycle(dtrn, epoch), lr=lr)
-    snapshot() = model(dtrn)
-    snapshots = (snapshot() for x in takenth(progress(training),length(dtrn)))
-    res = collect(flatten(snapshots))
-    return res
+function val_loss(g::GCN,x,y)
+    output = g(x)[:, idx_val]
+    nll(output, y[idx_val]) + (o[:weight_decay] * sum(g.layer1.w .* g.layer1.w))
 end
-results = train_with_results(model, dtrn, arguments.epochs, arguments.lr)
-trnloss = Array{Float32}(results)
-plot(trnloss, ylim=(0.0,2.0),labels=[:trnloss],xlabel="Epochs",ylabel="Loss")
-output = model(features)
-accuracy(output[:,idx_train], labels_decoded[idx_train])
-output = model(features)
-accuracy(output[:,idx_test], labels_decoded[idx_test])
+function val_loss(g::GCN, d)
+    mean(val_loss(g,x,y) for (x,y) in d)
+end
+
+function test_loss(g::GCN,x,y)
+    output = g(x)[:, idx_test]
+    nll(output, y[idx_test]) + (o[:weight_decay] * sum(g.layer1.w .* g.layer1.w))
+end
+function test_loss(g::GCN,d)
+    mean(test_loss(g,x,y) for (x,y) in d)
+end
+
+(g::GCN)(x,y) = nll(g(x)[:, idx_train], y[idx_train]) + (o[:weight_decay] * sum(g.layer1.w .* g.layer1.w))
+
+
+function main(args=ARGS)
+    # Handling arguments
+    s = ArgParseSettings()
+    s.description="train.jl"
+
+    s.exc_handler=ArgParse.debug_handler
+    @add_arg_table! s begin
+        ("--dataset"; arg_type=AbstractString; default="cora"; help="the name of the dataset")
+        ("--epochs"; arg_type=Int; default=200; help="number of epochs for training")
+        ("--lr"; arg_type=Float64; default=0.01; help="learning rate")
+        ("--weight_decay"; arg_type=Float64; default=5e-4; help="weight_decay")
+        ("--hidden"; arg_type=Int; default=16; help="hidden")
+        ("--pdrop"; arg_type=Float64; default=0.5; help="pdrop")
+        ("--window_size"; arg_type=Int; default=10; help="window_size")
+        ("--atype"; default=(Knet.gpu()>=0 ? "KnetArray{Float32,2}" : "SparseMatrixCSC{Float32,Int64}"); help="array type: Array for cpu, KnetArray for gpu")
+    end
+
+    isa(args, AbstractString) && (args=split(args))
+    if in("--help", args) || in("-h", args)
+        ArgParse.show_help(s; exit_when_done=false)
+        return
+    end
+
+    o = parse_args(args, s; as_symbols=true)
+    atype = eval(Meta.parse(o[:atype]))
+
+    # Load dataset
+    adj, features, labels, idx_train, idx_val, idx_test = load_dataset(o[:dataset])
+    model = GCN(size(features,1),
+                o[:hidden],
+                size(labels,2),
+                adj,
+                o[:pdrop])
+
+    labels_decoded = mapslices(argmax, labels ,dims=2)[:]
+
+    data =  minibatch(features, labels_decoded[:], length(labels_decoded))
+
+    #if
+    iters, trnloss, vallos = mytrain!(model, data, o[:epochs], o[:lr], o[:window_size])
+    plot(iters, [trnloss, vallos] , xlim=(1:3),labels=[:trn :val :tst], xlabel="epochs", ylabel="loss")
+    #else
+
+    #end
+    png(o[:dataset])
+
+    output = model(features)
+    print(accuracy(output[:,idx_train], labels_decoded[idx_train]))
+    print(accuracy(output[:,idx_test], labels_decoded[idx_test]))
+
+end
+PROGRAM_FILE=="train.jl" && main(ARGS)
+end
--- gcn/utils.jl
+++ gcn/utils.jl
@@ -19,15 +19,18 @@ def parse_index_file(filename):
         index.append(int(line.strip()))
     return index
 
-
 def sample_mask(idx, l):
-
     mask = np.zeros(l)
     mask[idx] = 1
     return np.array(mask, dtype=np.bool)
 
+def save_sparse_csr(filename, array):
+    np.savez(filename, data=array.data, indices=array.indices, indptr=array.indptr, shape=array.shape)
+def load_sparse_csr(filename):
+    loader = np.load(filename + '.npz')
+    return sp.csr_matrix((loader['data'], loader['indices'], loader['indptr']), shape=loader['shape'])
 
-def load_data(dataset_str):
+def load_data(dataset_str, atype):
     names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
     objects = []
     for i in range(len(names)):
@@ -52,13 +55,7 @@ def load_data(dataset_str):
         ty_extended[test_idx_range-min(test_idx_range), :] = ty
         ty = ty_extended
 
-    ##########################################
     if dataset_str == 'nell':
-        def save_sparse_csr(filename, array):
-            np.savez(filename, data=array.data, indices=array.indices, indptr=array.indptr, shape=array.shape)
-        def load_sparse_csr(filename):
-            loader = np.load(filename + '.npz')
-            return sp.csr_matrix((loader['data'], loader['indices'], loader['indptr']), shape=loader['shape'])
         test_idx_range_full = range(allx.shape[0], len(graph))
         isolated_node_idx = np.setdiff1d(test_idx_range_full, test_idx_reorder)
         tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
@@ -84,8 +81,6 @@ def load_data(dataset_str):
         else:
             features = load_sparse_csr("data/{}.features".format(dataset_str))
 
-    ##############################################
-
     else:
         features = sp.vstack((allx, tx)).tolil()
         features[test_idx_reorder, :] = features[test_idx_range, :]
@@ -93,57 +88,33 @@ def load_data(dataset_str):
     adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
 
     labels = np.vstack((ally, ty))
-    labels[test_idx_reorder, :] = labels[test_idx_range, :]
+    labels[test_idx_reorder,
+     :] = labels[test_idx_range, :]
 
     idx_test = test_idx_range.tolist()
     idx_train = range(len(y))
     idx_val = range(len(y), len(y)+500)
 
-    train_mask = sample_mask(idx_train, labels.shape[0])
-    val_mask = sample_mask(idx_val, labels.shape[0])
-    test_mask = sample_mask(idx_test, labels.shape[0])
-
-    y_train = np.zeros(labels.shape)
-    y_val = np.zeros(labels.shape)
-    y_test = np.zeros(labels.shape)
-    y_train[train_mask, :] = labels[train_mask, :]
-    y_val[val_mask, :] = labels[val_mask, :]
-    y_test[test_mask, :] = labels[test_mask, :]
-
-    return adj, np.transpose(features), labels, y_train, y_val, y_test, train_mask, val_mask, test_idx_reorder
+    return adj, np.transpose(features), labels, idx_train, idx_val, idx_test
     """
 
-    adj, features, labels, y_train, y_val, y_test, train_mask, val_mask, test_idx_reorder = py"load_data"(dataset)
+    adj, features, labels, idx_train, idx_val, idx_test = py"load_data"(dataset)
+
+    # Zero-indexing issue
+    idx_train = idx_train .+ 1
+    idx_val = idx_val .+ 1
+    idx_test = idx_test .+ 1
 
     (I, J, V) = scipy_sparse_find(adj)
     # Zero-indexing issue
     adj = sparse(I .+ 1, J .+ 1, V)
-    adj = convert(Array{Float32,2}, adj)
-    #adj = KnetArray(adj)
-    #adj = Array(adj)
 
     (I, J, V) = scipy_sparse_find(features)
     # Zero-indexing issue
     features = sparse(I .+ 1, J .+ 1, V)
-    features = convert(Array{Float32,2}, features)
-    #features = KnetArray(features)
-    #features = Array(features)
 
-    # TODO: Uncomment the following
-    # Normalize feature
-    # features = features./sum(features,2)
-    # Add identity matrix
-    #adj += sparse(I, size(adj,1), size(adj,2))
-    # Normalize
-    #adj = adj./sum(adj,2)
+    adj = convert(atype, adj)
+    features = convert(atype features)
 
-    #adj = convert(KnetArray{Float32,2}, adj)
-    #features = convert(KnetArray{Float32,2}, features)
-    #labels = convert(KnetArray{Float32,2}, labels)
-
-    idx_train = 1:140
-    idx_val = 200:499
-    #idx_test = 500:size(features,2)
-    idx_test = 500:1499
-    return adj, features, labels, idx_train, idx_val, test_idx_reorder
+    return adj, features, labels, idx_train, idx_val, idx_test
 end
